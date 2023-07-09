5cd2701df9e0de59fb8585a7f357d6a84197a3a0
Tim Long <timuism@me.com>
2023-07-09 19:12:24 -0400

    try dynamic viewport js fix

--- src/components/PanelContent.vue
+++ src/components/PanelContent.vue
@@ -1,28 +1,41 @@
 <script setup lang="ts">
-import { computed, ref, toRefs, watch, watchEffect } from 'vue'
+import { computed, onMounted, ref, toRefs, watch, watchEffect } from 'vue'
 import { useScroll } from "@vueuse/core"
 import { store } from '../store';
 import type { Ref } from 'vue'
 
 const { headerHeight, topOffset } = toRefs(store)
-const content:Ref<HTMLElement | null> = ref(null)
+const content: Ref<HTMLElement | null> = ref(null)
 const contentHeight = ref(-1)
 const { arrivedState } = useScroll(content)
 
+onMounted(() => {
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  let vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+  window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+})
+
 watchEffect(async () => {
-  if(!content.value) return
+  if (!content.value) return
   contentHeight.value = window.innerHeight - topOffset.value - headerHeight.value
   window.addEventListener('resize', () => {
     contentHeight.value = window.innerHeight - topOffset.value - headerHeight.value
   })
 
   window.addEventListener('scroll', (e) => {
-    contentHeight.value = window.innerHeight - topOffset.value - headerHeight.value 
+    contentHeight.value = window.innerHeight - topOffset.value - headerHeight.value
   })
 })
 
 watch(arrivedState, (newArrivedState) => {
-  console.log({aS: newArrivedState})
+  console.log({ aS: newArrivedState })
 })
 
 const $contentHeight = computed(() => {
@@ -34,6 +47,13 @@ const $contentHeight = computed(() => {
 
 <template>
   <section ref="content" :style="$contentHeight" class="overflow-y-auto">
-      <slot />
+    <slot />
   </section>
 </template>
+
+<style scoped>
+section {
+  height: 100vh;
+  height: calc(var(--vh, 1vh) * 100);
+}
+</style>
